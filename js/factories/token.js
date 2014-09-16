d660d3b6071586d0af90c2bf91b491159bd8f7f0
miclle <miclle.zheng@gmail.com>
2014-09-16 21:47:39 +0800

    Update token scope

--- js/factories/token.js
+++ js/factories/token.js
@@ -4,7 +4,7 @@ var tube = angular.module("tube");
 
 tube.factory('tokenFactory', ['$http', 'base64', 'servicesConfig', function($http, base64, servicesConfig) {
 
-  var url = servicesConfig.demoService + "/token?grant_type=password&scope=*,scope://sg/_demo&email=qcasting-demo@qgenius.com&password=demo";
+  var url = servicesConfig.demoService + "/token?grant_type=password&scope=*,scope://qcasting/_demo&email=qcasting-demo@qgenius.com&password=demo";
 
   $http.defaults.headers.common.Authorization = 'Basic ' + base64.encode('democlient:NDFkZGJjMDUtNzJmNi00');
 
